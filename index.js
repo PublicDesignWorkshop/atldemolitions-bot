9975a29d233dabd38a173abe16ec9408ae3b07bd
ericrav <ericrabinowitz@me.com>
2016-07-28 16:09:31 -0400

    break code into functions and add logging

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ var oauthOptions = {
   json: true,
   headers: {
     'content-type': 'application/x-www-form-urlencoded',
-    'postman-token': 'ec06ed9b-fc6a-7390-994f-484e6b192319',
     'cache-control': 'no-cache'
   },
   form: accelaConfig
@@ -22,7 +21,7 @@ var oauthOptions = {
 
 // make the request to Accela API using the options above
 request(oauthOptions, function (error, response, body) {
-  if (error) throw new Error(error);
+  if (error) console.error('error getting access token', error);
 
   var accela_token = body.access_token;
   var yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD') + ' 00:00:00';
@@ -35,7 +34,6 @@ request(oauthOptions, function (error, response, body) {
     url: 'https://apis.accela.com/v4/search/records/',
     qs: { expand: 'addresses' },
     headers: {
-      'postman-token': 'bae3fae3-45ad-5d31-0564-f7bd862da239',
       'cache-control': 'no-cache',
       'content-type': 'application/json',
       authorization:  accela_token
@@ -51,7 +49,7 @@ request(oauthOptions, function (error, response, body) {
 
   // make the actual search request
   request(searchOptions, function (error, response, body) {
-    if (error) throw new Error(error);
+    if (error) console.error('error making search', error);
 
     for (var i = 0; i < body.result.length; i++) {
       var record = body.result[i];
@@ -63,53 +61,63 @@ request(oauthOptions, function (error, response, body) {
       console.log(status);
 
       // don't tweet all the result at once by creating a time delay
-      staggerTweet(address, status, i * 1000 * 60 * 3);
+      staggerTweet(address, status, i * 1000 * 60 * 5);
     }
   });
 });
 
 function staggerTweet(street, status, delay) {
-  setTimeout(function() { createTweet(street, status) }, delay);
+  setTimeout(function() { getStreetViewImage(street, status) }, delay);
 }
 
-
-function createTweet(street, status) {
+function getStreetViewImage(street, status) {
   var location = encodeURI(street + ', Atlanta, GA');
   // get a street view image from the location using Google Maps API
   request.get('https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + location, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      // load the preexisint red x image into Jimp
-      Jimp.read('cross.png')
-      .then(function(cross) {
-        // load the streetview image into Jimp
-        Jimp.read(new Buffer(body))
-        .then(function(image) {
-          // place the red X image on top of the street view image
-          image.composite(cross, 0, 0);
-          // get a buffer of the image so it can be uploaded to Twitter
-          image.getBuffer(Jimp.MIME_JPEG, function(err, buffer) {
-            // upload the buffer to Twitter so the file can be attached to a tweet
-            Bot.post('media/upload', { media_data: new Buffer(buffer).toString('base64') }, function (err, data, response) {
-              console.log('upload')
-              console.log(err)
-              var mediaIdStr = data.media_id_string
-              var meta_params = { media_id: mediaIdStr }
-            
-
-              Bot.post('media/metadata/create', meta_params, function (err, data, response) {
-                console.log(err);
-                if (!err) {
-                  // now we can reference the media and post a tweet (media will attach to the tweet) 
-                  var params = { status: status, media_ids: [mediaIdStr] }
-                  Bot.post('statuses/update', params, function (err, data, response) {
-                    console.log('done');
-                  });
-                }
-              })
-            })
-          })
-        });
-      })
+    if (error) {
+      console.error('error getting streetview', error);
+    } else {
+      // request body is received as image buffer
+      addCrossToImageBuffer(body);
     }
+  });
+}
+
+function addCrossToImageBuffer(streetviewBuffer) {
+  // load the preexisint red x image into Jimp
+  Jimp.read('cross.png')
+  .then(function(cross) {
+    // load the streetview image into Jimp
+    Jimp.read(new Buffer(streetviewBuffer))
+    .then(function(image) {
+      // place the red X image on top of the street view image
+      image.composite(cross, 0, 0);
+      // get a buffer of the image so it can be uploaded to Twitter
+      image.getBuffer(Jimp.MIME_JPEG, function(err, buffer) {
+        if (err) console.error('error getting Jimp image buffer', err);
+        else createTweetWithImage(buffer);
+      })
+    });
+  });
+}
+
+function createTweetWithImage(buffer) {
+  // upload the buffer to Twitter so the file can be attached to a tweet
+  Bot.post('media/upload', { media_data: new Buffer(buffer).toString('base64') }, function (err, data, response) {
+    if (err) console.error('error uploading image to Twitter', err);
+    var mediaIdStr = data.media_id_string
+    var meta_params = { media_id: mediaIdStr }
+    Bot.post('media/metadata/create', meta_params, function (err, data, response) {
+      if (err) {
+        console.error('error creating metadata', err);
+      } else {
+        // now we can reference the media and post a tweet (media will attach to the tweet) 
+        var params = { status: status, media_ids: [mediaIdStr] }
+        Bot.post('statuses/update', params, function (err, data, response) {
+          if (err) console.error('error creating tweet', err);
+          else console.log('done tweeting');
+        });
+      }
+    })
   });
 }
